8a2bc3aa4a7e1d3913c3a8ddb909c35d9a3f17d1
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-13 12:27:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 let dodger = document.getElementById("dodger")
 function move(){
-  document.addEventListener("keyup",(e) => {
+  document.addEventListener("keydown",(e) => {
     if(e.key === "ArrowLeft"){
       let nmb = dodger.style.left.replace("px","")
       let left = parseInt(nmb,10)
