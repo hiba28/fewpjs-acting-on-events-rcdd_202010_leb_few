d07eef1fbb462611093a7c258c05fbf1c23be4b5
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-13 12:17:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function move(){
       if(right < 400)
         dodger.style.right = `${right + 2}px`
     }
-    else if(e.key === "ArrowRight"){
-      let nmb = dodger.style.right.replace("px","")
+    else if(e.key === "ArrowUp"){
+      let nmb = dodger.style.top.replace("px","")
       let right = parseInt(nmb,10)
       if(right < document.style.width)
         dodger.style.right = `${right + 2}px`
