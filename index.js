9cb3447e3afa115b4dc91d71b0d5ef4e8d42ef3a
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-13 12:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+function move(){
+  document.addEventListener("keydown",(e) => {
+    if(e.key === "ArrowLeft"){
+      let nmb = dodger.style.left.replace("px","")
+      let left = parseInt(nmb,10)
+      if(left > 0)
+        dodger.style.left = `${left - 2}px`
+    }
+    if(e.key === "ArrowRight"){
+      let nmb = dodger.style.right.replace("px","")
+      let right = parseInt(nmb,10)
+      if(right < 400)
+        dodger.style.right = `${right + 2}px`
+    }
+  })
+}
+move()
